1c92b071a69388be1970469aa96605f4c75b7a3d
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-31 21:47:48 +0530

    added buttons

--- src/App.js
+++ src/App.js
@@ -10,6 +10,31 @@ function App() {
           <div className='display'>
             <input type="text" value={valueN} />
           </div>
+          <div>
+            <input type="button" value="AC" onClick={e=> setValue("")}/>
+            <input type="button" value="DE" onClick={e=> setValue(valueN.slice(0, -1))}/>
+            <input type="button" value="." onClick={e=> {
+              if(valueN[valueN.length - 1]==="-" || valueN[valueN.length - 1]==="+" || valueN[valueN.length - 1]==="/" || valueN[valueN.length - 1]==="*" || valueN[valueN.length - 1]===".")
+              {
+                setValue(valueN.slice(0, -1))
+              }
+              else
+              {
+                setValue(valueN + e.target.value)
+              }
+            }}/>
+            <input type="button" value="/" onClick={e=> {
+              console.log(valueN[valueN.length - 1])
+              if(valueN[valueN.length - 1]==="-" || valueN[valueN.length - 1]==="+" || valueN[valueN.length - 1]==="/" || valueN[valueN.length - 1]==="*")
+              {
+                setValue(valueN.slice(0, -1) + e.target.value)
+              }
+              else
+              {
+                setValue(valueN + e.target.value)
+              }
+            }}/>
+          </div>
 
         </form>
       </div>
